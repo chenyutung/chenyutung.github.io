1d5870e350919ab2c60287f66bf771974b5373ef
chenyutung <cheny333@newschool.edu>
2022-03-04 11:35:02 -0500

    aa

--- project2/question.js
+++ project2/question.js
@@ -1,30 +1,131 @@
-console.clear();
-
-const typing = document.querySelectorAll('.typewriter');
-
-function type(element) {
-
-function randomOpacity() {
-  return (Math.floor(Math.random() * 50) + 50)/100;
-}
-
-function randomEms() {
-  if (Math.random() > .8) {
-  return (Math.floor(Math.random() * 100) - 50)/800;
-  }
-  else {
-    return 0;
-  }
-}
-
-function wrap(char) {
-  return '<span style="opacity:' + randomOpacity() + '; text-shadow:' + randomEms() + 'em ' + randomEms() + 'em currentColor;">' + char + '</span>';
-}
-
-const wrappedText = Array.from(element.textContent).map(wrap);
-
-element.innerHTML = wrappedText.join('');
+(function emojiCursor() {
   
-}
-
-typing.forEach(type);
+    var possibleEmoji = ["❓", "🧐", "😵‍💫", "❔"]
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var cursor = {x: width, y: width};
+    var particles = [];
+    
+    function init() {
+      bindEvents();
+      loop();
+    }
+    
+    // Bind events that are needed
+    function bindEvents() {
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('touchmove', onTouchMove);
+      document.addEventListener('touchstart', onTouchMove);
+      
+      window.addEventListener('resize', onWindowResize);
+    }
+    
+    function onWindowResize(e) {
+      width = window.innerWidth;
+      height = window.innerHeight;
+    }
+    
+    function onTouchMove(e) {
+      if( e.touches.length > 0 ) {
+        for( var i = 0; i < e.touches.length; i++ ) {
+          addParticle( e.touches[i].clientX, e.touches[i].clientY, possibleEmoji[Math.floor(Math.random()*possibleEmoji.length)]);
+        }
+      }
+    }
+    
+    function onMouseMove(e) {    
+      cursor.x = e.clientX;
+      cursor.y = e.clientY;
+      
+      addParticle( cursor.x, cursor.y, possibleEmoji[Math.floor(Math.random()*possibleEmoji.length)]);
+    }
+    
+    function addParticle(x, y, character) {
+      var particle = new Particle();
+      particle.init(x, y, character);
+      particles.push(particle);
+    }
+    
+    function updateParticles() {
+      
+      // Updated
+      for( var i = 0; i < particles.length; i++ ) {
+        particles[i].update();
+      }
+      
+      // Remove dead particles
+      for( var i = particles.length -1; i >= 0; i-- ) {
+        if( particles[i].lifeSpan < 0 ) {
+          particles[i].die();
+          particles.splice(i, 1);
+        }
+      }
+      
+    }
+    
+    function loop() {
+      requestAnimationFrame(loop);
+      updateParticles();
+    }
+    
+    /**
+     * Particles
+     */
+    
+    function Particle() {
+  
+      this.lifeSpan = 120; //ms
+      this.initialStyles ={
+        "position": "absolute",
+        "display": "block",
+        "pointerEvents": "none",
+        "z-index": "10000000",
+        "fontSize": "16px",
+        "will-change": "transform"
+      };
+  
+      // Init, and set properties
+      this.init = function(x, y, character) {
+  
+        this.velocity = {
+          x:  (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2),
+          y: 1
+        };
+        
+        this.position = {x: x - 10, y: y - 20};
+  
+        this.element = document.createElement('span');
+        this.element.innerHTML = character;
+        applyProperties(this.element, this.initialStyles);
+        this.update();
+        
+        document.body.appendChild(this.element);
+      };
+      
+      this.update = function() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.lifeSpan--;
+        
+        this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + (this.lifeSpan / 120) + ")";
+      }
+      
+      this.die = function() {
+        this.element.parentNode.removeChild(this.element);
+      }
+      
+    }
+    
+    /**
+     * Utils
+     */
+    
+    // Applies css `properties` to an element.
+    function applyProperties( target, properties ) {
+      for( var key in properties ) {
+        target.style[ key ] = properties[ key ];
+      }
+    }
+    
+    init();
+  })();
