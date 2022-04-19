3bd0386c2303256ba89fb321d308382e97b57d9b
chenyutung <cheny333@newschool.edu>
2022-04-19 19:27:42 -0400

    e

--- Collection/collection.js
+++ Collection/collection.js
@@ -1,5 +1,7 @@
 const div = document.querySelector('div');
 const ul = document.querySelector('ul');
+const li = document.querySelector('li');
+let container = document.querySelector(".container");
 
 async function getPeople(){ 
   let response = await fetch('https://api.airtable.com/v0/apprQovZ8QrIy14da/Form%20Responses%201?api_key=key8XafwyxvEVD9sn'); 
@@ -10,18 +12,43 @@ getPeople().then(data => { makeList(data.records) });
 
 function makeList(records) { 
   for(let i = 0; i < records.length; i++) { 
+    let song = records[i].fields.song; 
+    let listItem = document.createElement('div'); 
+    let songimg = records[i].fields.songimg;
+
+    listItem.classList.add("pname");
+
+
+    let moodnumber = records[i].fields.moodnumber; 
+    let moodnumberItem = document.createElement('div'); 
+    moodnumberItem.classList.add("moodnumber");
+    moodnumberItem.innerHTML = moodnumber;
+
+    let genre = records[i].fields.genre; 
+    let genreItem = document.createElement('div'); 
+    genreItem.classList.add("genre");
+    genreItem.innerHTML = genre;
+
     let name = records[i].fields.name; 
-    let listItem = document.createElement('div'); 
-    listItem.appendChild(document.createTextNode(name)); 
-    div.appendChild(listItem); 
-  } 
+    let nameItem = document.createElement('div'); 
+    nameItem.classList.add("name");
+    nameItem.innerHTML = name;
 
-  for(let i = 0; i < records.length; i++) { 
-    let age = records[i].fields.age; 
-    let listItem = document.createElement('div'); 
-    listItem.appendChild(document.createTextNode(age)); 
-    div.appendChild(listItem);   
-  } 
+  
+    
 
+    
+    listItem.appendChild(document.createTextNode(songimg+song));
+    listItem.appendChild( moodnumberItem); 
+    listItem.appendChild(genreItem);
+    listItem.appendChild(nameItem);
+    
+
+    container.appendChild(listItem); 
+
+   
+
+    
+  }
   
 }
